b3cdab915b70f0c4e04ba93c5c9a814d42e29001
Robert Marianski <rob@marianski.com>
2015-07-23 12:09:49 -0400

    Support custom gunicorn log format

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,9 +4,11 @@ default[:tileserver][:user][:enabled] = true
 default[:tileserver][:cfg_path] = '/etc/tileserver'
 default[:tileserver][:run_path] = '/var/run/tileserver'
 
+default[:tileserver][:gunicorn][:enabled] = true
 default[:tileserver][:gunicorn][:worker_class] = 'gevent'
 default[:tileserver][:gunicorn][:worker_processes] = node[:cpu][:total] * 2 + 1
-default[:tileserver][:gunicorn][:enabled] = true
+# default access log format
+default[:tileserver][:gunicorn][:access_log_format] = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 default[:tileserver][:runit][:svwait] = 180
 
