27780050f4771cbf5e9327e68078411fca1e7ef2
Matt Amos <matt.amos@mapzen.com>
2017-11-09 11:09:15 +0000

    Update package versions to latest.

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,35 +22,34 @@ default[:tileserver][:revision][:vector_datasource] = 'master'
 
 default[:tileserver][:pip_requirements] = %w(
   appdirs==1.4.3
-  argparse==1.2.1
-  boto==2.33.0
-  future==0.15.2
+  argparse==1.4.0
+  boto==2.48.0
+  future==0.16.0
   hiredis==0.2.0
-  Jinja2==2.8
-  MarkupSafe==0.23
-  ModestMaps==1.4.6
-  protobuf==2.6.0
-  psycopg2==2.5.4
-  pyclipper==1.0.5
-  pycountry==1.20
+  Jinja2==2.9.6
+  MarkupSafe==1.0
+  ModestMaps==1.4.7
+  protobuf==3.4.0
+  psycopg2==2.7.3.2
+  pyclipper==1.0.6
+  pycountry==17.9.23
   pyproj==1.9.5.1
-  python-dateutil==2.4.2
-  PyYAML==3.11
-  redis==2.10.5
-  requests==2.10.0
-  Shapely==1.4.3
-  simplejson==3.6.4
-  six==1.10.0
+  python-dateutil==2.6.1
+  PyYAML==3.12
+  redis==2.10.6
+  requests==2.18.4
+  Shapely==1.6.2.post1
+  six==1.11.0
   StreetNames==0.1.5
   ujson==1.35
-  Werkzeug==0.9.6
+  Werkzeug==0.12.2
   wsgiref==0.1.2
-  zope.dottedname==4.1.0
-  git+https://github.com/ixc/python-edtf@aad32b8d5cd8848c50fbef92c73697a93cf182ba#edtf
+  zope.dottedname==4.2
+  edtf==2.6.0
+  mapbox-vector-tile==1.2.0
 )
 
 default[:tileserver][:pip_requirements] += [
-  "git+https://github.com/tilezen/mapbox-vector-tile@#{node[:tileserver][:revision][:mapbox_vector_tile]}#egg=mapbox-vector-tile",
   "git+https://github.com/tilezen/raw_tiles@#{node[:tileserver][:revision][:raw_tiles]}#egg=raw_tiles",
   "git+https://github.com/tilezen/tilequeue@#{node[:tileserver][:revision][:tilequeue]}#egg=tilequeue",
   "git+https://github.com/tilezen/tileserver@#{node[:tileserver][:revision][:tileserver]}#egg=tileserver"
